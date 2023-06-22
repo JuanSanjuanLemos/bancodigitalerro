0220318494f4ea39fa9b6cc071fd475642bb1a61
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2023-06-22 00:11:40 -0300

    22

--- banco.js
+++ banco.js
@@ -1,12 +1,13 @@
-// const readline = require('readline').createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// })
-let password = 12345;
+// instalar pacote inquirer npm install inquirer
+// a fazer: colocar nome na tela, imprimir valor ao sair(precisa perguntar se o usuário deseja, posso usar a biblioteca), exigir senha para sair
+import { input,select  } from '@inquirer/prompts';
+let password = 1;
 
-const User = {
+const User = 
+{
   fullName: "",
   name: "",
+  password:"",
   balance: "",
   isLogged:0,
   firstAccess: 1,
@@ -21,68 +22,115 @@ const User = {
     if(this.isLogged)
       return this.fullName.split[0];
     else
-      return "O usuário precisa estar logado para realizar essa ação!";
+      console.log("O usuário precisa estar logado para realizar essa ação!");
   },
   Deposit(value){
     if(this.isLogged){
       this.balance += value;
       this.transactions.push({type:"Depósito", value:value});
+      console.log(`Depósito efetuado no valor de R$ ${value}.\nSaldo da conta R$ ${firstUser.balance}`);
     }
     else
-      return "O usuário precisa estar logado para realizar essa ação!";
+      console.log("O usuário precisa estar logado para realizar essa ação!");
   },
   WithDraw(value){
     if(this.isLogged){
-      if(this.balance > 1 && value<this.balance){
-        this.balance -= value;
-        this.transactions.push({type:"Saque", value:value});
+      if(this.balance>=value)
+      {
+        if(value<=1)
+          console.log("O valor mínimo para saque é R$ 1,00");
+        else{
+          this.balance -= value;
+          this.transactions.push({type:"Saque", value:value});
+          console.log(`Saque efetuado no valor de R$ ${value}.\nSaldo da conta R$ ${this.balance}`);
+        }
       }
       else
-        return "Saldo insuficiente!";
+        console.log("Saldo insuficiente!");
     }
     else
-      return "O usuário precisa estar logado para realizar essa ação!";
+      console.log("O usuário precisa estar logado para realizar essa ação!");
   },
-  ShippingTransfer(nameReceiver,value){
+  Transfer(nameReceiver,value){
     if(this.isLogged){
-      if(this.balance > 1 && value<this.balance){
-        this.balance -= value;
-        this.transactions.push({type:"Envio de transferência", 
-        nameShipping:this.fullName, 
-        nameReceiver:nameReceiver, 
-        value:value
-      });
+      if(value<=this.balance){
+        if(value<=1)
+          console.log("O valor mínimo para transferência é R$ 1,00");
+        else{
+          this.balance -= value;
+          this.transactions.push({type:"Envio de transferência", 
+          nameShipping:this.fullName, 
+          nameReceiver:nameReceiver, 
+          value:value
+          });
+          console.log(`Transferência efetuada no valor de R$ ${value}.
+          \nNome depositante: ${firstUser.fullName}
+          \nNome recebedor: ${nameReceiver}
+          \nSaldo da conta R$ ${firstUser.balance}`);
+        }
       }
       else
-        return "Saldo insuficiente!";
+      console.log("Saldo insuficiente!");
     }
     else
-      return "O usuário precisa estar logado para realizar essa ação!";
-  },
-  ReceiptTransfer(nameShipping,value){
-    if(this.isLogged){
-      this.balance += value;
-      this.transactions.push(
-        {type:"Recebimento de transferência", 
-        nameShipping:nameShipping, 
-        nameReceiver: this.fullName,
-        value:value
-      });
-    }
-    else
-      return "O usuário precisa estar logado para realizar essa ação!";
+    console.log("O usuário precisa estar logado para realizar essa ação!");
   }
 }
 const firstUser = User;
 while(!firstUser.isLogged){
-  firstUser.fullName = "Juan Lemos";
-  let passwordUser = 123;
-  if(password === passwordUser){
+ firstUser.fullName = await input({message:"Nome:"});
+ firstUser.password = await input({message:"Senha:"});
+  if(firstUser.password == password){
     firstUser.isLogged = 1;
   }
   else 
     console.log("Senha incorreta!");
 }
 while(firstUser.isLogged){
-  return "Logou!";
+  const answer = await select({
+    message: 'Selecione uma ação',
+    choices: [
+      {
+        name: 'Saque',
+        value: 'Saque'
+      },
+      {
+        name: 'Depósito',
+        value: 'Depósito'
+      },
+      {
+        name: 'Transferência',
+        value: 'Transferência'
+      },
+      {
+        name: 'Saldo',
+        value: 'Saldo'
+      },
+      {
+        name: 'Sair',
+        value: 'Sair'
+      }
+    ],
+  });
+  let value;
+  switch(answer){
+    case 'Depósito':
+      value = parseFloat(await input({message:"Valor:"}));
+      firstUser.Deposit(value);
+      break;
+    case 'Saque':
+      value = parseFloat(await input({message:"Valor:"}));
+      firstUser.WithDraw(value);
+      break;
+    case 'Transferência':
+      value = parseFloat(await input({message:"Valor:"}));
+      let nameReceiver = await input({message:"Nome do recebedor:"});
+      firstUser.Transfer(nameReceiver,value);
+      break;
+    case 'Saldo':
+        console.log(`Saldo: R$ ${firstUser.balance}`);
+        break;
+    case 'Sair':
+        firstUser.isLogged = 0;
+  }
 }
